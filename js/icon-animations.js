b4120b70d765dcd52733147ace7673a4ccea7870
Robert Schneiderman <Rob@roberts-mbp-2.gateway.sonic.net>
2016-10-03 20:30:16 -0700

    fixed random assortment for finished product of splash bg

--- js/icon-animations.js
+++ js/icon-animations.js
@@ -5,19 +5,21 @@
   };
 
   const mri = (frm, to) => {
-    return (Math.random() * to) + frm;
+    return (Math.random() * (to - frm)) + frm;
   };  
 
   const randomBinary = () => {
     return mr(1, 2);
   };
 
+  window.mri = mri;
+
   const randomDimensions = (icon) => {
-    let val = mri(3, 5);
+    let val = mri(6, 8);
     icon.style.width = `${val}rem`;   
     icon.style.height = `${val}rem`;       
-    // icon.style.width = `5.5rem`;   
-    // icon.style.height = `5.5rem`;    
+    // icon.style.width = `8rem`;   
+    // icon.style.height = `8rem`;    
   };
 
   const randomOrigin = (icon) => {
@@ -72,8 +74,20 @@
 
   const placeIconRandomly = (icon) => {
     icon.style.left = `${mr(0, 1300)}px`;
-    icon.style.top = `${mr(0, 700)}px`;
+    icon.style.top = `${mr(0, 1800)}px`;    
   } 
+
+  const sizeDown = (icon) => {
+    debugger;
+    let width = icon.style.width;
+    let height = icon.style.height;
+    icon.style.width = `${(width.slice(0, width.length - 2) - 1)}rem`;
+    icon.style.width = `${(height.slice(0, height.length - 2) - 1)}rem`;
+  }
+
+  const dimensionInteger = (dim) => {
+    return dim.slice(0, dim.length - 2);
+  }
 
   const overlap = (newVec, oldVecs) => {
     newVec = newVec.getBoundingClientRect();
@@ -99,38 +113,81 @@
     let icon =  icons[i];
     randomDimensions(icon);
 
-    placeIconRandomly(icon);
+    placeIconRandomly(icon);    
 
-    debugger;
+    if (i === icons.length) debugger;
 
     iconsExcluding = icons.slice(0, i).concat(icons.slice(i+1));
 
+    let j = 0;
     while(overlap(icon, iconsExcluding)) {
-      placeIconRandomly(icon);      
+      // debugger;
+      if ((j >= 3) && (dimensionInteger(icon.style.width) > 6.5)) {
+        sizeDown(icon);
+        j = 0;
+      }
+      if (dimensionInteger(icon.style.width) < 6.5) {
+        icon.style.width = `4rem`
+        icon.style.height = `4rem`
+      }      
+      placeIconRandomly(icon);
+      j++
     }
     
   };
 
-  for (let i = 0; i < icons.length; i++) {
-    // let time = (icons[i].getBoundingClientRect().top / 500);
+  // const animateIcons = () => {
+    for (let i = 0; i < icons.length; i++) {
+      let time = (icons[i].getBoundingClientRect().top / 500);
 
-    TweenMax.to(icons[i], 7, {
-      top: "-=500",
-      rotation: `+=${mr(50, 150)}`,
-      ease: Linear.easeNone,
-      repeat: -1,
-    }, 0);    
-  }  
+      // icons[i].getBoundingClientRect().top
 
-  // const iconRise = () => {
-  //   tm.to(icons, 7, {
-  //     top: "-=500",
-  //     rotation: `+=${mr(50, 150)}`,
-  //     ease: Linear.easeNone,
-  //     repeat: 0,
-  //   }, 0);
+      TweenMax.to(icons[i], 32, {
+        top: "-=1900",
+        rotation: `+=${mr(350, 450)}`,
+        ease: Linear.easeNone,
+        repeat: -1,
+      }, 0);    
+    }
   // }
 
-  // iconRise();
+  // setInterval(animateIcons, 28000);
+
+  let k = 0
+  setInterval(() => {
+    console.log("k:", 1);
+    k++
+  }, 1000);
+
+
+  // jQuery('img.icon').each(function(){
+  //     var $img = jQuery(this);
+  //     var imgID = $img.attr('id');
+  //     var imgClass = $img.attr('class');
+  //     var imgURL = $img.attr('src');
+
+  //     jQuery.get(imgURL, function(data) {
+  //         // Get the SVG tag, ignore the rest
+  //         var $svg = jQuery(data).find('svg');
+
+  //         // Add replaced image's ID to the new SVG
+  //         if(typeof imgID !== 'undefined') {
+  //             $svg = $svg.attr('id', imgID);
+  //         }
+  //         // Add replaced image's classes to the new SVG
+  //         if(typeof imgClass !== 'undefined') {
+  //             $svg = $svg.attr('class', imgClass+' replaced-svg');
+  //         }
+
+  //         // Remove any invalid XML tags as per http://validator.w3.org
+  //         $svg = $svg.removeAttr('xmlns:a');
+
+  //         // Replace image with new SVG
+  //         $img.replaceWith($svg);
+
+  //     }, 'xml');
+
+  // });  
+
 
 })();
