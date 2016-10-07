168cafe0d55ed4fcef27037a0f3560df04005d5c
Robert Schneiderman <Rob@Roberts-MacBook-Pro-2.local>
2016-10-07 10:55:45 -0700

    added facebook share btn animation

--- js/icon-animations.js
+++ js/icon-animations.js
@@ -155,36 +155,6 @@
   //   k++
   // }, 1000);
 
-
-  // jQuery('img.icon').each(function(){
-  //     var $img = jQuery(this);
-  //     var imgID = $img.attr('id');
-  //     var imgClass = $img.attr('class');
-  //     var imgURL = $img.attr('src');
-
-  //     jQuery.get(imgURL, function(data) {
-  //         // Get the SVG tag, ignore the rest
-  //         var $svg = jQuery(data).find('svg');
-
-  //         // Add replaced image's ID to the new SVG
-  //         if(typeof imgID !== 'undefined') {
-  //             $svg = $svg.attr('id', imgID);
-  //         }
-  //         // Add replaced image's classes to the new SVG
-  //         if(typeof imgClass !== 'undefined') {
-  //             $svg = $svg.attr('class', imgClass+' replaced-svg');
-  //         }
-
-  //         // Remove any invalid XML tags as per http://validator.w3.org
-  //         $svg = $svg.removeAttr('xmlns:a');
-
-  //         // Replace image with new SVG
-  //         $img.replaceWith($svg);
-
-  //     }, 'xml');
-
-  // });
-
   const creationWaypoint = () => {
     let waypoint = new Waypoint({
       element: document.getElementById('ot-creation'),
@@ -220,6 +190,22 @@
       offset: '75%'    
     }); 
   };
+
+  const shareBtnWaypoint = () => {
+    new Waypoint({
+      element: document.getElementById('fb-share-btn'),
+      handler: function() {
+        this.element.style.visibility = 'visible';        
+        this.element.classList.add('zoomInDown');
+        setTimeout(() => {
+          this.element.classList.add('tada');
+        }, 500);
+        this.destroy();
+      },
+      offset: '95%'    
+    }); 
+  }
+  shareBtnWaypoint();
 
   const textWaypoint = () => {
     Array.from(document.getElementsByClassName('how-to-text')).forEach((el, i) => {
